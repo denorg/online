03da2fd7572ec573d4987ed0b9d7850aa1d5fd61
Anand Chowdhary <mail@anandchowdhary.com>
2020-05-15 05:09:19 +0530

    :sparkles: Add package

--- mod.ts
+++ mod.ts
@@ -1,4 +1,9 @@
-/** JSDoc for this line */
-export function mode() {
-  return 0;
+/** Check if you are online and connected to the internet */
+export async function isOnline(
+  { timeout, family }: { timeout?: number; family?: number },
+): Promise<boolean> {
+  const text =
+    await (await (await fetch("http://captive.apple.com/hotspot-detect.html"))
+      .blob()).text();
+  return (text ?? "").toLowerCase().includes("success");
 }
